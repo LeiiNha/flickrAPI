5fba7f05255e79938c820319180443e5c78cc04e
Erica Geraldes <ericageraldes@Ericas-MacBook-Pro.local>
2019-07-18 19:41:15 +0100

    First commit

---
+++ FlickrAPISample/FlickrAPISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrAPISample
+//
+//  Created by Erica Geraldes on 18/07/2019.
+//  Copyright © 2019 Erica Geraldes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
